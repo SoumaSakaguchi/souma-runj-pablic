adaec706f15e7fe56aa9d0f9ceffd7b9bb8ce50b
SoumaSakaguchi <mokemoke1319@gmail.com>
2023-11-27 14:23:43 +0900

    3rd commit: Test cobra.command in create_netns.go

--- cmd/runj/create_netns.go
+++ cmd/runj/create_netns.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"os"
-	"os/exec"
-
-	"go.sbk.wtf/runj"
+	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
-func main(){
+func create_netnsCommand() *cobra.Command{
+	create_netns := &cobra.Command{
+		Use: "crate_netns",
+		Short: "Create a new vnet jail like Network Namesapce.",
+		PreRunE: func() {
+			fmt.Printf("Test create_netns.go");
+		},
+	}
 }
