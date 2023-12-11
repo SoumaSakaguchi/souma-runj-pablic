50934aaea23894f6884e73610929e8f343331009
SoumaSakaguchi <mokemoke1319@gmail.com>
2023-12-11 18:52:14 +0900

    add setNetnsConf()

--- oci/config_netns.go
+++ oci/config_netns.go
@@ -1,7 +1,28 @@
 package oci
 
 import (
+	"encoding/json"
+	"os"
+
+	"go.sbk.wtf/runj/runtimespec"
 )
 
-func setNetnsConf() {
+func setNetnsConf() (*runtimespec.Spec, error) {
+	config := *runtimespec.Spec{}
+
+	config.Version = runtimespec.Version
+	config.Process = {
+		false,
+		["/usr/bin/sh"],
+		["PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"]
+	}
+	cofig.Root = {
+		"/"
+	}
+	config.Hostname = "netns"
+	config.FreeBSD = {
+		{nil,{"new",nil}}
+	}
+
+	return config, nil
 }
