f87d991b33c7a4c6f3267d33cc1c989265a60990
SoumaSakaguchi <mokemoke1319@gmail.com>
2024-01-16 21:10:53 +0900

    netns/link.go: Add LoadSymlink()

--- netns/link.go
+++ netns/link.go
@@ -11,10 +11,28 @@ const (
 	netnsLink = "netns"
 )
 
-func CreateSymlink(jailID string, netnsID string) (error) {
+func CreateSymlink(jailID string, netnsID string) error {
 	err := os.Symlink(filepath.join(state.Dir(jailID), netnsLink), NsDir(netnsID))
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func LoadSymlink(path string) (string, error) {
+	info, err := os.Lstat(path)
+	if err != nil {
+		return "", err
+	}
+
+	var realPath string
+	if info.Mode()&os.ModeSymlink == os.ModeSymlink {
+		realPath, err = os.Readlink(path)a
+		if err != nil{
+			return "", err
+		}
+	} else {
+		return "", fmt.Errorf("Path is not symboliclink")
+	}
+	return filepath.Base(realPath), nil
 }
